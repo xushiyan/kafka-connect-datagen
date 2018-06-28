ed3250414eb367b26d20ba423eecada8453e638e
XU SHIYAN <xu.shiyan.raymond@gmail.com>
2018-06-28 19:25:11 +0800

    Add placeholder for unit test

---
+++ src/test/java/com/github/xushiyan/kafka/connect/datagen/DatagenTaskTest.java
@@ -0,0 +1,12 @@
+package com.github.xushiyan.kafka.connect.datagen;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DatagenTaskTest {
+    @Test
+    public void trueTest() {
+        assertTrue(DatagenTask.class instanceof Object);
+    }
+}
